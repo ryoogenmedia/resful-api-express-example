6adc44f76dcb5b85ba30b8be5ce19846ffba45ab
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2025-01-05 09:02:26 +0800

    [dev] error handler

---
+++ src/middleware/error-middleware.ts
@@ -0,0 +1,26 @@
+import { User } from "@prisma/client";
+import { Request, Response, NextFunction } from "express";
+import { ZodError } from "zod";
+import { ResponseError } from "../error/response-error";
+
+export const errorMiddleware = async (
+  user: User,
+  req: Request,
+  res: Response,
+  error: Error,
+  next: NextFunction
+) => {
+  if (error instanceof ZodError) {
+    res.status(400).json({
+      errors: `Validation error : ${JSON.stringify(error)}`,
+    });
+  } else if (error instanceof ResponseError) {
+    res.status(error.status).json({
+      errors: error.message,
+    });
+  } else {
+    res.status(500).json({
+      errors: error.message,
+    });
+  }
+};
