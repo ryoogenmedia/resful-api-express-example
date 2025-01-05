38f0870326e500db7a894e761c0c00cb1be9ce9f
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2025-01-05 11:34:47 +0800

    [dev] routes multi role

--- src/router/admin-api.ts
+++ src/router/admin-api.ts
@@ -1,9 +1,7 @@
 import express from "express";
-import { authMiddleware } from "../middleware/auth-middleware";
 import { adminMiddleware } from "../middleware/admin-middleware";
 
 export const adminRouter = express.Router();
 
 // ADMIN API CHECK AUTHENTICATION & ROLES USING MIDDLEWARE
-adminRouter.use(authMiddleware);
 adminRouter.use(adminMiddleware);
--- src/router/user-api.ts
+++ src/router/user-api.ts
@@ -1,9 +1,7 @@
 import express from "express";
-import { authMiddleware } from "../middleware/auth-middleware";
 import { userMiddleware } from "../middleware/user-middleware";
 
 export const userRouter = express.Router();
 
 // USER API CHECK AUTHENTICATION USING MIDDLEWARE
-userRouter.use(authMiddleware);
 userRouter.use(userMiddleware);
