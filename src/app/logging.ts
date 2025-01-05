154a4720f841bc59991649817ab0268ec0607650
Bintang. <90763575+M-BintangR@users.noreply.github.com>
2025-01-05 08:35:40 +0800

    chore: application setup

---
+++ src/app/logging.ts
@@ -0,0 +1,16 @@
+import winston from "winston";
+
+export const logger = winston.createLogger({
+  level: "debug",
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.printf(({ timestamp, level, message }) => {
+      if (typeof message === "object") {
+        message = JSON.stringify(message, null, 2);
+      }
+      return `${timestamp} [${level}]: ${message}`;
+    })
+  ),
+  transports: [new winston.transports.Console()],
+});
